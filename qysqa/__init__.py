b8fc0e7e5dc31eebe13b28ea7cc0b2ae02936f8f
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-01-04 20:16:14 +0600

    ✅ Change config structure

--- qysqa/__init__.py
+++ qysqa/__init__.py
@@ -5,25 +5,24 @@ from flask import Flask
 from flask.cli import with_appcontext
 from flask_sqlalchemy import SQLAlchemy
 
-from qysqa.config import get_config
+from qysqa.config import SECRET_KEY
 
 __version__ = (0, 1, 0, "dev")
 
 db = SQLAlchemy()
-config = get_config()
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    if (db_url := config.get("db_url")) is None:
-        db_path = os.path.join(app.instance_path, "db.sqlite3")
-        db_url = f"sqlite:///{db_path}"
-        # ensure the instance folder exists
-        os.makedirs(app.instance_path, exist_ok=True)
+    # if (db_url := config.DB_URL) is None:
+    db_path = os.path.join(app.instance_path, "db.sqlite3")
+    db_url = f"sqlite:///{db_path}"
+    # ensure the instance folder exists
+    os.makedirs(app.instance_path, exist_ok=True)
 
     app.config.from_mapping(
-        SECRET_KEY=config["secret_key"],
+        SECRET_KEY=SECRET_KEY,
         SQLALCHEMY_DATABASE_URI=db_url,
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
