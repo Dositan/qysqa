bb3887d964edade2866c3731e12f0ec3951f9569
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-01-13 00:37:31 +0600

    ✨ Configuration improvements

--- app/config.py
+++ app/config.py
@@ -1,3 +1,27 @@
 import secrets
+import os
+from dotenv import load_dotenv
 
-SECRET_KEY = secrets.token_urlsafe(48)
+load_dotenv()
+
+
+class BaseConfig:
+    SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_urlsafe(48))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("DEV_DATABASE_URL")
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
+    USE_RELOADER = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+
+config = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+}
