674812c611bef8b8d5ed3d837b63adddb4852006
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-01-22 01:59:12 +0600

    ✅ Add `Flask-Login`, `Flask-Bcrypt` dependencies

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,10 @@
 """Extensions module. Each extension is initialized in the app factory."""
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 csrf_protect = CSRFProtect()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
