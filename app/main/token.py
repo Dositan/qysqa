f50c97845bf269213dfa1747290e7721d0eb6650
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-15 21:33:01 +0600

    fix(*): format files

--- app/main/token.py
+++ app/main/token.py
@@ -9,7 +9,7 @@ TOKEN_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz_-"
 
 def generate_token():
     """A way to generate random, unique 6-digit tokens.
-    
+
     Has a DB check whether the generated token already exists.
     """
     while True:
